f7c499cefac9e17a20143e5e77d40f1fbc0c1f63
Eki Zulkarnaen <eki.zulkarnaen@gmail.com>
2020-08-10 08:44:33 +0700

    Add try catch to PostConstruct

--- src/main/java/com/ekz/shopeeng/PostConstructBean.java
+++ src/main/java/com/ekz/shopeeng/PostConstructBean.java
@@ -19,18 +19,22 @@ public class PostConstructBean {
 	
 	@PostConstruct
     public void init() {
-		List<Product> list = productService.findProductList();
-		
-		if(list.size() == 0){
-			for(int i=0;i<5;i++){
-				Product prd = new Product();
-				prd.setProductName(MyUtils.randomString(5, false));
-				prd.setProductDesc(MyUtils.randomString(20, false));
-				prd.setProductImage(MyUtils.randomString(15, false));
-				prd.setPrice(MyUtils.randomNumber(5000, 100000));
-				
-				productService.save(prd);
+		try{
+			List<Product> list = productService.findProductList();
+			
+			if(list.size() == 0){
+				for(int i=0;i<5;i++){
+					Product prd = new Product();
+					prd.setProductName(MyUtils.randomString(5, false));
+					prd.setProductDesc(MyUtils.randomString(20, false));
+					prd.setProductImage(MyUtils.randomString(15, false));
+					prd.setPrice(MyUtils.randomNumber(5000, 100000));
+					
+					productService.save(prd);
+				}
 			}
+		}catch(Exception e){
+			System.out.println("PostConstruct is not ready");
 		}
     }
 }
